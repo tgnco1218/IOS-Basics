0cdcb66f8c25272bb8047f1b6a13fe263ced3b26
Yi-Ting Kuo <tgnco1218@yahoo.com.tw>
2019-12-24 13:19:12 +0800

    Add files via upload

---
+++ iOSAppPractice/BasicLessons/AutoLayoutExercises3/AutoLayoutExercises3/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  AutoLayoutExercises3
+//
+//  Created by KYT on 2019/12/23.
+//  Copyright © 2019 tgnco1218. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
