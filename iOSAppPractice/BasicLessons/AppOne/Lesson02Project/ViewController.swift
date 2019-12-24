d204b9d55b9972cab02454d44ba26818a0951b06
Yi-Ting Kuo <tgnco1218@yahoo.com.tw>
2019-12-24 13:15:14 +0800

    Add files via upload

---
+++ iOSAppPractice/BasicLessons/AppOne/Lesson02Project/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Lesson02Project
+//
+//  Created by KYT on 2019/12/16.
+//  Copyright © 2019 tgnco1218. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
